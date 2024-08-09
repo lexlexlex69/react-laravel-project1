260c2f61b77e0abfbdd55d2b54211bed4cf93e95
lex <you@example.com>
2024-08-09 17:12:15 +0800

    added front end crud

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -25,12 +25,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:55',
             'email' => 'required|email|unique:users,email'.$this->id,
-            'password' => [
-                'confirmed',
-                Password::min(8)
-                ->letters()
-                ->symbols()
-            ]
+            'password' => 'string'
         ];
     }
 }
--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axiosClient from "../axios-client";
-
+import { Link } from "react-router-dom";
 export default function Users() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -15,11 +15,86 @@ export default function Users() {
             .get("/users")
             .then(({ data }) => {
                 setLoading(false);
+                setUsers(data.data);
                 console.log(data);
             })
             .catch(() => {
                 setLoading(false);
             });
     };
-    return <div>Users</div>;
+
+    const onDelete = (u) => {
+        if (!window.confirm("Are you sure u want to delete this user?")) {
+            return;
+        }
+
+        axiosClient.delete("/users/" + u.id).then(() => {
+            getUsers();
+        });
+    };
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+            >
+                <h1>Users</h1>
+                <Link to="/users/new" className="btn-add">
+                    Add New
+                </Link>
+            </div>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created At</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    {loading && (
+                        <tbody>
+                            <tr>
+                                <td colSpan={5} className="text-center">
+                                    Loading...
+                                </td>
+                            </tr>
+                        </tbody>
+                    )}
+                    {!loading && (
+                        <tbody>
+                            {users.map((u) => (
+                                <tr>
+                                    <td>{u.id}</td>
+                                    <td>{u.name}</td>
+                                    <td>{u.email}</td>
+                                    <td>{u.created_at}</td>
+                                    <td>
+                                        <Link
+                                            className="btn-edit"
+                                            to={"/users/" + u.id}
+                                        >
+                                            Edit
+                                        </Link>
+                                        &nbsp;
+                                        <button
+                                            onClick={() => onDelete(u)}
+                                            className="btn-delete"
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    )}
+                </table>
+            </div>
+        </div>
+    );
 }
